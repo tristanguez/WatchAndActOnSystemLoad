4461123a76559fb5a3ba458ea419ee0772e3f831
Tristan Guez <tristan.guez@gmail.com>
2024-02-04 20:42:58 -0500

    Reading an ini file

--- SuiviChargeSysteme.lua
+++ SuiviChargeSysteme.lua
@@ -1,5 +1,41 @@
 -- Suivi de charge systeme
 --
+
+--
+--Utilities
+--
+function ini_read(FileName)
+	local inifile = io.open(FileName, "r")
+
+	if inifile == nil then
+		return {}
+	end
+
+	local couples = {}
+
+	for line in inifile:lines() do
+		local key, value = line:match("(%S+)%s*=%s*(%S+)") -- S: non-space s: space +: at least one *: 0 or more
+
+		if key then
+			if value == nil then
+				value = ""
+			end
+			couples[key] = tonumber(value) or value
+			print(key .. "=" .. value)
+		end
+	end
+
+	return couples
+end
+
+--
+--
+-- Main
+--
+--
+
+local inits = ini_read("SuiviChargeSysteme.ini")
+
 local file = io.open("/proc/loadavg", "r")
 
 if file == nil then
